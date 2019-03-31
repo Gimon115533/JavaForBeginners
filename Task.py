802a88aa7aa92d170d49aba23b982bcfa5d6001c
Gimon115533 <razvilka2009@yandex.ru>
2019-03-31 20:15:59 +0300

    исправил замечания

--- Task.py
+++ Task.py
@@ -14,15 +14,16 @@ class Search(unittest.TestCase):
         elm.send_keys('selenide')
         elm.send_keys(Keys.RETURN)
         assert 'No results found' not in self.drv.page_source
-        spisok = self.drv.find_element_by_xpath('//*[@id="rso"]/div[1]/div/div[1]/div/div').text
-        assert 'selenide.org' in spisok
+        spisok = self.drv.find_element_by_xpath('//*[@id="rso"]/div[1]/div/div/div/div/div[1]/a/div/cite')
+        assert 'selenide.org' in spisok.text
         baton_more = self.drv.find_element_by_xpath('//*[@id="ow16"]')
         baton_more.click()
         img = self.drv.find_element_by_xpath('//*[@id="lb"]/div/a[1]')
         img.click()
         assert 'selenide' in self.drv.find_element_by_xpath('//*[@id="rg_s"]/div[1]/a[2]').text
         self.drv.find_element_by_xpath('//*[@id="hdtb-msb-vis"]/div[1]/a').click()
-        assert 'selenide.org' in spisok
+        spisok2 = self.drv.find_element_by_xpath('//*[@id="rso"]/div[1]/div/div/div/div/div[1]/a/div/cite')
+        assert 'selenide.org' in spisok2.text
 
     def tearDown(self):
         self.drv.close()
